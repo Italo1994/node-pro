a34012469dd67e7f817ef860f47e22b31bd16669
Italo1994 <italohortiz2015@gmail.com>
2021-11-23 02:04:37 -0300

    Criando o método post, put e delete do CRUD

--- Authentication/src/index.ts
+++ Authentication/src/index.ts
@@ -3,12 +3,18 @@ import usersRouter from './routes/users.route';
 
 const app = express();
 
+// Configurações da Aplicação
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Configurações de Rotas
 app.use(usersRouter);
 
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
     res.status(200).send( { foo: 'bar' } );
 });
 
+// Inicialização do Servidor
 app.listen(3000, () => {
     console.log('Aplicação executando na porta 3000');
 });
--- Authentication/src/routes/users.route.ts
+++ Authentication/src/routes/users.route.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response, NextFunction } from 'express';
+import { StatusCodes } from 'http-status-codes';
 
 const usersRouter = Router();
 
@@ -10,7 +11,28 @@ usersRouter.get('/users', (req: Request, res: Response, next: NextFunction) => {
 usersRouter.get('/users/:uuid', (req: Request<{uuid: string}>, res: Response, next: NextFunction) => {
     const uuid = req.params.uuid;
 
-    res.status(200).send( {uuid} );
+    res.status(StatusCodes.OK).send( {uuid} );
+});
+
+usersRouter.post('/users', (req: Request, res: Response, next: NextFunction) => {
+    const newUser = req.body;
+
+    console.log(req.body);
+
+    res.status(StatusCodes.CREATED).send(newUser);
+});
+
+usersRouter.put('/users/:uuid', (req: Request, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+    const modifiedUser = req.body;
+
+    modifiedUser.uuid = uuid;
+
+    res.status(StatusCodes.OK).send({ modifiedUser });
+});
+
+usersRouter.delete('/users/:uuid', (req: Request, res: Response, next: NextFunction) => {
+    res.status(StatusCodes.OK);
 });
 
 export default usersRouter;
